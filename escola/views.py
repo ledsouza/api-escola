dc57dd1e1fb59a6d2a3a087385f1d449b74c7c14
ledsouza <leandro.souza.159@gmail.com>
2024-06-05 11:55:07 -0300

    atualizando resposta Location no header

--- escola/views.py
+++ escola/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, status
+from rest_framework.response import Response
 from escola.models import Aluno, Curso, Matricula
 from escola.serializer import AlunoSerializer, AlunoSerializerV2, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
 
@@ -21,6 +22,16 @@ class CursosViewSet(DefaultHttpMethods, viewsets.ModelViewSet):
     """Exibindo todos os cursos"""
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = Response(
+                serializer.data, status=status.HTTP_201_CREATED)
+            id = str(serializer.data['id'])
+            response['Location'] = request.build_absolute_uri() + id
+            return response
 
 
 class MatriculaViewSet(DefaultHttpMethods, viewsets.ModelViewSet):
